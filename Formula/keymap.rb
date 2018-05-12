80df61ce40ae57fa22c4e6ef11621110abb830c1
Ivan Greene <ivan@ivan.sh>
2018-05-12 15:03:45 -0500

    v0.1.2

--- Formula/keymap.rb
+++ Formula/keymap.rb
@@ -1,9 +1,9 @@
 class Keymap < Formula
   desc "remap keyboard keys from the command line"
   homepage "https://github.com/ivangreene/keymap"
-  url "https://github.com/ivangreene/keymap/archive/v0.1.1.tar.gz"
-  version "v0.1.1"
-  sha256 "89a8044e96e13772432b737d57540bb631480c970de6b36195c798dff3bb699a"
+  url "https://github.com/ivangreene/keymap/archive/v0.1.2.tar.gz"
+  version "v0.1.2"
+  sha256 "93c4837d81af9f47098a479dc82e74f9bd8dc09a6be36369dee75ad7b8f39ace"
 
   def install
     bin.install "keymap.py" => "keymap"
