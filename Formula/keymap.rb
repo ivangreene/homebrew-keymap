8da8bce845fabba2a582bb1a0332dd7a20c20bd3
Ivan Greene <ivan@ivan.sh>
2024-05-24 05:23:27 -0400

    Upgrade to v0.2.2 (#1)
    * Upgrade to v0.2.2
    
    * Fix audit problems

--- Formula/keymap.rb
+++ Formula/keymap.rb
@@ -1,9 +1,8 @@
 class Keymap < Formula
-  desc "remap keyboard keys from the command line"
+  desc "Remap keyboard keys from the command-line"
   homepage "https://github.com/ivangreene/keymap"
-  url "https://github.com/ivangreene/keymap/archive/v0.2.1.tar.gz"
-  version "v0.2.1"
-  sha256 "d314b2860e04e2580e7af55832eb22eb7b90197163b01722ebcbb0a5862b6dcc"
+  url "https://github.com/ivangreene/keymap/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "e4ba0724391dc28b2c3e50ba167317aa3a567a2df27ac8932f51896b30fd2638"
   depends_on "python3"
 
   def install
@@ -13,7 +12,7 @@ class Keymap < Formula
   end
 
   test do
-    system "which hidutil"
-    system "python #{prefix}/test.py"
+    system "which", "hidutil"
+    system "python", "#{prefix}/test.py"
   end
 end
