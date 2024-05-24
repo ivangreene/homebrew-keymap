3a0d0c1ef8ab726ca349d74859c1ac60af3616a0
Ivan Greene <ivan@ivan.sh>
2024-05-24 10:14:34 +0100

    Fix audit problems

--- Formula/keymap.rb
+++ Formula/keymap.rb
@@ -1,8 +1,7 @@
 class Keymap < Formula
-  desc "remap keyboard keys from the command line"
+  desc "Remap keyboard keys from the command-line"
   homepage "https://github.com/ivangreene/keymap"
   url "https://github.com/ivangreene/keymap/archive/refs/tags/v0.2.2.tar.gz"
-  version "v0.2.2"
   sha256 "e4ba0724391dc28b2c3e50ba167317aa3a567a2df27ac8932f51896b30fd2638"
   depends_on "python3"
 
@@ -13,7 +12,7 @@ class Keymap < Formula
   end
 
   test do
-    system "which hidutil"
-    system "python #{prefix}/test.py"
+    system "which", "hidutil"
+    system "python", "#{prefix}/test.py"
   end
 end
